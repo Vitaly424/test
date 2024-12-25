5cb26b16f6fa41dfc8e5d8fa9715b3a74f71a92a
Vitaly424 <v1t19982@yandex.ru>
2024-12-25 23:17:59 +0300

    .

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { MantineProvider } from '@mantine/core';
+
+import '@mantine/core/styles.css';
+
+import { Router } from './Router';
+import { theme } from './theme';
+
+export default function App() {
+  return (
+    <MantineProvider theme={theme}>
+      <Router />
+    </MantineProvider>
+  );
+}
